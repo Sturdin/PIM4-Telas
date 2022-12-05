a6cb8b7ca0a363eabccea1495be7716360b1ca6d
Sturdin <107217156+Sturdin@users.noreply.github.com>
2022-12-04 23:51:15 -0300

    Initial commit

---
+++ app/inf_beneficiario/src/main/java/com/pim4/inf_beneficiario/inf_beneficiario.java
@@ -0,0 +1,14 @@
+package com.pim4.inf_beneficiario;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class inf_beneficiario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inf_pagamentos);
+    }
+}
---
+++ app/src/main/java/com/pim4/pagamentos/inf_pagamentos.java
@@ -0,0 +1,14 @@
+package com.pim4.pagamentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inf_pagamentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inf_pagamentos);
+    }
+}
